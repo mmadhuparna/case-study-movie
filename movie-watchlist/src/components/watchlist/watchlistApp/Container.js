64e5b54f34056829e5f8db6983fdfc8cf5e82af6
mmadhuparna <mmadhuparna@yahoo.com>
2018-12-25 19:12:49 +0530

    commit

--- movie-watchlist/src/components/watchlist/watchlistApp/Container.js
+++ movie-watchlist/src/components/watchlist/watchlistApp/Container.js
@@ -9,21 +9,30 @@ class AppContainer extends Component {
   state = {
     showMovies: true,
     movies: [],
+    showError: true
   }
 //add movie to watchlist
-  _addMovie = (movie) => {
+  _addMovie = (movie) => 
+  {let index = this.state.movies.findIndex(el => el.id == movie.id);
+    if(index == -1)
     this.setState((prevState) => {
       const movies = [movie, ...prevState.movies];
-      return { movies };
+      return { movies }
+      
     });
+    {
+      this.setState((prevState) => {
+      return { showError: !prevState.showError }
+    })
+    }
   }
 //delete movie from watchlist
   _deleteMovie(id) {
     console.log(id);
-    this.setState(prevState => {
-      const movies = prevState.movies.filter(movie => movie.id !== id);
-      return { movies };
-    });
+      const movies = this.state.movies.filter(movie => 
+      {return movie.id !== id ;}
+    );
+    this.setState({movies})
   }
 
   render() {
